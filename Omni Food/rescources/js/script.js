2bb2aacd631627be17e200a442db9b3a0896da88
hayek <hayek@HANNA-PC>
2018-12-18 23:41:00 +0800

    64. Building a “sticky” navigation - Part 2

--- Omni Food/rescources/js/script.js
+++ Omni Food/rescources/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
- 
+    //  here we add the . to class name    
+    $('.js--section-features').waypoint(function(direction){
+    if(direction == "down"){
+        $('nav').addClass('sticky');
+    }else{
+        $('nav').removeClass('sticky');
+    }
+
+ }, {
+    offset: '60px'
+  });
+
+
+
+
+
 });
