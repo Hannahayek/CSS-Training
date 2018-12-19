e8597974aa3cde68e240fdc9041300c855e00f3b
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2018-12-19 17:18:39 +0800

    65. Scrolling to elements

--- Omni Food/rescources/js/script.js
+++ Omni Food/rescources/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    
+    // Sticky Navigation
     //  here we add the . to class name    
     $('.js--section-features').waypoint(function(direction){
     if(direction == "down"){
@@ -10,9 +12,27 @@ $(document).ready(function(){
  }, {
     offset: '60px'
   });
+/* Scroll on buttons */
+$('.js--scroll-to-plans').click(function(){
+  $('html,body').animate({scrollTop:$('.js--section-plans').offset().top},1000);
+});
 
-
-
-
-
+$('.js--scroll-to-start').click(function(){
+    $('html,body').animate({scrollTop:$('.js--section-features').offset().top},1000);
+  });
+/* navigiation scrolling  */
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 });
