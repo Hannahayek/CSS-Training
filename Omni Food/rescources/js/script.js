cdaf23ee2aef12e211787c16df5c59e806a22cd5
hayek <hayek@HANNA-PC>
2018-12-19 21:59:24 +0800

    67. Making the navigation responsive

--- Omni Food/rescources/js/script.js
+++ Omni Food/rescources/js/script.js
@@ -61,5 +61,21 @@ $('.js--wp-4').waypoint(function(direction){
     offset:'50%'
 })
 
+/* Mobile Nav  */
+$('.js--nav-icon').click(function(){
+var nav=$('.js--main-nav');;
+var icon=$(".js--nav-icon i")
+
+nav.slideToggle(200);
+if(icon.hasClass('ion-navicon-round')){
+    icon.addClass('ion-close-round');
+    icon.removeClass('ion-navicon-round');
+}else{
+    icon.removeClass('ion-close-round');
+    icon.addClass('ion-navicon-round');
+}
+
+});
+
 
 });
